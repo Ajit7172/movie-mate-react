ffee18fb928627aeca9e939490cd4ac12ce4570a
Ajit Kumar Chaudhary <chaudharyajit7172@gmail.com>
2024-06-08 13:28:18 +0545

    learned about CSS & Bootstrap

---
+++ bootstrap/css.md
@@ -0,0 +1,25 @@
+css = cascading style sheet
+
+
+
+Tablet
+mobile
+pc
+monitor
+
+
+
+framework
+
+-> inline styling (tv, tablet, mobile, pc....)
+=>SASS (syntactically awesome style sheet)
+       => dynamic programming (color, font, logo, font family)
+
+       => Component based framework (SASS + Component)
+            => component (table, card, navbar, footer, loading)
+            =>bootstrap, material ui, chakra UI, antdesign
+
+=> utitility based framework (Tailwind)
+            => CSS wrapped garera utility (css short cut)
+            -> tools: tailwind, radix ui, shadecn/ui (super popular)
+                        
--- html/index.js
+++ html/index.js
@@ -1,64 +1,13 @@
-// Write a factorial for 5
-const factorial = n => {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-};
+/*
+3. let arr1 = ['icecream', 'pizza', 'momo'];
+   let arr2 = ['fruits', 'pizza', 'veggies'];
+   find the common item in both arrays
+   result ['pizza']
+*/ 
 
-console.log(factorial(5)); // Output: 120
-
-// HW
-// Write a fibonaci series for nth Number
-
-// create a book object and do CRUD operations (spread operator)
-let books = {
-  1: { title: "1984", author: "George Orwell", year: 1949 },
-  2: { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-  3: { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 }
-};
-
-// Create
-const createBook = (books, newBook) => {
-  const id = Object.keys(books).length + 1;
-  return { ...books, [id]: newBook };
-};
-
-books = createBook(books, { title: "Brave New World", author: "Aldous Huxley", year: 1932 });
-console.log("After Create:", books);
-
-// Read
-const readBook = (books, id) => {
-  return books[id] || null;
-};
-
-console.log("Read Book ID 2:", readBook(books, 2));
-
-// Update
-const updateBook = (books, id, updatedBook) => {
-  if (!books[id]) {
-    return books; // If the book doesn't exist, return the original books object
-  }
-  return { ...books, [id]: { ...books[id], ...updatedBook } };
-};
-
-books = updateBook(books, 3, { year: 1926 });
-console.log("After Update:", books);
-
-// Delete
-const deleteBook = (books, id) => {
-  const { [id]: _, ...remainingBooks } = books;
-  return remainingBooks;
-};
-
-books = deleteBook(books, 1);
-console.log("After Delete:", books);
-
-
-const reverseArray = arr => arr.slice().reverse();
-
-const inputArray = [1, 2, 3, 4, 5];
-const reversedArray = reverseArray(inputArray);
-
-console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
-console.log(inputArray); // Output: [1, 2, 3, 4, 5] (remains unchanged)
+   let arr1 = ['icecream', 'pizza', 'momo'];
+   let arr2 = ['fruits', 'pizza', 'veggies'];
+   
+   // implicit function
+   const getCommon = (a1, a2) => a1.filter((item) => a2.includes(item));
+   console.log(getCommon(arr1, arr2));
